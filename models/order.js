24cf7a1f0d3d528292780c29afc952e58474c98f
damario789 <damario789@gmail.com>
2021-07-27 14:10:04 +0700

    Add table and seed

--- models/order.js
+++ models/order.js
@@ -3,21 +3,21 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
+  class Order extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Category.hasMany(models.Menu)
+      Order.belongsToMany(models.Menu, {through: models.OrderMenu})
     }
   };
-  Category.init({
-    categoryName: DataTypes.STRING
+  Order.init({
+    customerName: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Category',
+    modelName: 'Order',
   });
-  return Category;
+  return Order;
 };
