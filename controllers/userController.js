0a7f157937df8bf82801214c956300a9e35b4653
Ahmad Mustain Billah <mustain.bilah@gmail.com>
2021-07-27 11:21:21 +0700

    added nodemailer for sending email for registration and password reset

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,8 @@
 const {User} = require('../models')
 const hashCompare = require('../helpers/verifyHash')
 const signUser = require('../helpers/generateToken')
-
+const sendEmail = require('../helpers/sendEmail')
+const hashPassword = require('../helpers/hashPassword')
 class Controller{
   static login = (req,res,next) =>{
     console.log(req.body)
@@ -41,11 +42,34 @@ class Controller{
   static register = (req,res,next) =>{
     const newUser = Object.assign({}, req.body)
     User.create(newUser)
-    .then((user) =>{
+    .then(async (user) =>{
+      await sendEmail(user.email,"Account registration",`Your account has been registered successfully, you can login through this link ${process.env.CLIENT_URL}`)
       res.status(201).json({message : `user with email ${user.email} has been registered`})
     })
     .catch((error) =>{
       next(error)
+    })
+  }
+
+  static resetPassword = (req, res, next) => {
+    const email = req.params.email
+    const newPassword = req.body.password
+    User.findOne({
+      where : { email}
+    })
+    .then(async(user) => {
+      if(user){
+        user.password = hashPassword(newPassword)
+        user.save()
+        await sendEmail(user.email,"Password changed!",`Your password has been reset successfully, you can login through this link ${process.env.CLIENT_URL}`)
+        res.status(200).json({message:"Password has been reset successfully"})
+      }
+      else{
+        res.status(404).json({ message : `User with ${email} is not found`})
+      }
+    })
+    .catch((err) => {
+      next(err)
     })
   }
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,6 +4,7 @@ const {multerMiddleware} = require('../middlewares/multer')
 const {postImage} = require('../middlewares/uploadImage')
 
 router.post('/login',Controller.login)
+router.post('/reset-password/:email',Controller.resetPassword)
 router.post('/register',multerMiddleware,postImage,Controller.register)
 
 
