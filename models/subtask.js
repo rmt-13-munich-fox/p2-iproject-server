5716ff52f7a492c6fbaf590df972e7d612974bb5
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-28 00:59:17 +0700

    Creating table association for Task and Subtask

--- models/subtask.js
+++ models/subtask.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Subtask.belongsTo(models.Task, { foreignKey: 'TaskId' })
     }
   };
   Subtask.init({
--- models/task.js
+++ models/task.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Task.hasMany(models.Subtask, { foreignKey: 'TaskId' })
     }
   };
   Task.init({
