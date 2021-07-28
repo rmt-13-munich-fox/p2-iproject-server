6195260cbd82a42ebd1a8a935961e9a0dd1f3f86
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-28 21:01:24 +0700

    Init socket io in skeleton code

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 const router = require('./routes')
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
+io.on('connection', (socket) => {
+  console.log('user connected');
+})
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+module.exports = io
