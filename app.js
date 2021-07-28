765c997c437b82c07c03d91fb2b0b158aef5609a
diman19 <yadiman19@gmail.com>
2021-07-29 00:31:52 +0700

    socketIo on server added

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = process.env.port;
+const httpServer = require("http").createServer(app);
 const cors = require('cors');
 const routes = require('./routers');
+const io = require("socket.io")(httpServer);
+
+io.on("connection", (socket) => {
+  console.log('user Connected')
+  socket.on('sendMessage', (data) => {
+
+    io.emit('broadcastMessage', data)
+  })
+})
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -11,4 +21,4 @@ app.use(express.json())
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Server listen on port ${port}`))
+httpServer.listen(port, () => console.log(`Server listen on port ${port}`))
