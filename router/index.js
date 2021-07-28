f5e68957317a3e3940c43f398c10199e57a53e9f
darrel2411 <tapsdarrel@gmail.com>
2021-07-29 04:17:57 +0800

    tambahan fitur currency

--- router/index.js
+++ router/index.js
@@ -5,8 +5,11 @@ const plan = require('./plan')
 const story = require('./story')
 const travel = require('./travel')
 const user = require('./user')
+const Controller = require('../controllers/controllerCurrency')
 
 
+router.post('/currency', Controller.currencyConvert)
+router.get('/currency/list', Controller.allCurrency)
 
 router.use('/', user)
 // console.log('masuk sini', 111)
