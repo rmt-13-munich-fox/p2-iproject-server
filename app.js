a29cc3ea6fecf699d34c26ecd61ff13eae418fb6
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-29 01:00:10 +0700

    Add new socket on

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ const port = 3000
 const router = require('./routes')
 const httpServer = require('http').createServer(app)
 const io = require('socket.io')(httpServer)
+const { Task } = require('./models')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(router)
 io.on('connection', (socket) => {
-  console.log('user connected');
+  socket.on('fetchTasks', () => {
+    Task.findAll()
+      .then(tasks => {
+        io.emit('tasks', tasks)
+      })
+  })
 })
+app.use(router)
 
 httpServer.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
