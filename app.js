b2c4d36ffc9dbf4342db82591663bad14a620e62
Garry <Garry@Ivys-MacBook-2.local>
2021-07-29 07:10:31 +0700

    Heroku deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const router = require('./routers')
 const request = require('request')
 const mainRouter = require ('./routers/index')
