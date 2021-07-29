a245ae359f20c8e9373bb7b29d2a69c8c87450de
nwrsadat <anwarsadat.juni@gmail.com>
2021-07-29 10:03:11 +0700

    First third party api added

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 const userRouter = require('./userRouter')
 const taskRouter = require('./taskRouter')
 const subtaskRouter = require('./subtaskRouter')
+const newsRouter = require('./newsRouter')
 const { authentication } = require('../middlewares/auth')
 
 router.use('/', userRouter)
 router.use(authentication)
+router.use('/news', newsRouter)
 router.use('/tasks', taskRouter)
 router.use('/subtasks', subtaskRouter)
 
