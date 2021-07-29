20fa4d1ae1baf29e24d44e235f68acdab0e912ca
Yahya Ayyash S <ayyashcihuy@gmail.com>
2021-07-29 09:34:50 +0700

    before deploying

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
     cors: {
       origin: "*",
-      methods: ["GET", "POST"]
+      methods: ["GET", "POST", "DELETE"]
     }
   });
 
